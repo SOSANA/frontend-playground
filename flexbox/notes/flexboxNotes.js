98d37e33532e42efc5ae33b2403b8924249f3144
SOSANA <thesosana@gmail.com>
2016-08-26 17:57:51 -0600

    updating notes on flexbox wrap

--- flexbox/notes/flexboxNotes.js
+++ flexbox/notes/flexboxNotes.js
@@ -3,7 +3,7 @@
  *  'a guide to flexbox':
  *    - src: https://css-tricks.com/snippets/css/a-guide-to-flexbox/
  *
- *  'display:flex':
+ *  'display: flex;':
  *    - display defines a flex container; inline or block depending on the
  *      given value. It enables a flex context for all its direct children
  *    - defines a container, you put aside floats, widths, and percentages
@@ -12,7 +12,7 @@
  *      - 'inline-flex', works like a inline or span elements where it only
  *        takes what it needs
  *
- *  'flex-direction:row':
+ *  'flex-direction: row | row-reverse | column | column-reverse;':
  *    - This establishes the main-axis, thus defining the direction flex items
  *      are placed in the flex container. Flexbox is (aside from optional
  *      wrapping) a single-direction layout concept. Think of flex items as
@@ -24,6 +24,15 @@
  *      - referd to main axis as going top to bottom
  *    - 'column-reverse': same as row-reverse but bottom to top
  *
+ *  'flex-wrap: nowrap | wrap | wrap-reverse;':
+ *    - By default, flex items will all try to fit onto one line. You can
+ *      change that and allow the items to wrap as needed with this property.
+ *      Direction also plays a role here, determining the direction new lines
+ *      are stacked in
+ *    - 'nowrap' (default): single-line / left to right in 'ltr'; right to
+        left in 'rtl'
+      - 'wrap': multi-line / left to right in 'ltr'; right to left in 'rtl'
+      - 'wrap-reverse': multi-line / right to left in 'ltr'; left to right in 'rtl'
  *  'vh':
  *    - stands for view height and has nothing to do with flexbox, gives it
  *      100% view height
