6d102a3f120944b726be36647dda82650d02f572
SOSANA <thesosana@gmail.com>
2016-08-26 17:12:17 -0600

    updating flex box notes

---
+++ notes/flexboxNotes.js
@@ -0,0 +1,31 @@
+/**
+ * Notes:
+ *  'a guide to flexbox':
+ *    - src: https://css-tricks.com/snippets/css/a-guide-to-flexbox/
+ *
+ *  'display:flex':
+ *    - display defines a flex container; inline or block depending on the
+ *      given value. It enables a flex context for all its direct children
+ *    - defines a container, you put aside floats, widths, and percentages
+ *    - it will stretch across your screen, by setting this it automatically
+ *      sets your child elements as 'flex items'
+ *      - 'inline-flex', works like a inline or span elements where it only
+ *        takes what it needs
+ *
+ *  'flex-direction:row':
+ *    - This establishes the main-axis, thus defining the direction flex items
+ *      are placed in the flex container. Flexbox is (aside from optional
+ *      wrapping) a single-direction layout concept. Think of flex items as
+ *      primarily laying out either in horizontal rows or vertical columns
+ *    - 'row' (default): left to right in 'ltr'; right to left in 'rtl'
+ *      - refered to main axis as going from left to right
+ *    - 'row-reverse': right to left in 'ltr'; left to right in 'rtl'
+ *    - 'column': same as row but top to bottom
+ *      - referd to main axis as going top to bottom
+ *    - 'column-reverse': same as row-reverse but bottom to top
+ *
+ *  'vh':
+ *    - stands for view height and has nothing to do with flexbox, gives it
+ *      100% view height
+ *
+ */
