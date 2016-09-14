1f2d66a1116fba62de70c0adc195715f020f0e12
SOSANA <thesosana@gmail.com>
2016-09-14 13:02:29 -0600

    adding new nav component dependency

--- flexbox/wesbos/project/01-project/client/components/app.js
+++ flexbox/wesbos/project/01-project/client/components/app.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import Nav from './nav';
 
 export default class App extends Component { // eslint-disable-line
   render() {
     return (
       <div>
+        <Nav />
         {this.props.children}
       </div>
     );
