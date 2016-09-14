6f1fcf48c9361881f7c91e906f4b77d8fde26898
SOSANA <thesosana@gmail.com>
2016-09-14 13:03:21 -0600

    removing unneed text, keeping component for future

--- flexbox/wesbos/project/01-project/client/components/welcome.js
+++ flexbox/wesbos/project/01-project/client/components/welcome.js
@@ -4,7 +4,7 @@ export default class Welcome extends Component { // eslint-disable-line
   render() {
     return (
       <div className="test">
-        Welcome to our slice of paradise
+
       </div>
     );
   }
