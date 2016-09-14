7c71ab39c3aef7d7958496f3cfa1c55174b9ae9c
SOSANA <thesosana@gmail.com>
2016-09-14 14:49:14 -0600

    creating header and footer components, updating app component dependencies

--- flexbox/wesbos/project/01-project/client/components/app.js
+++ flexbox/wesbos/project/01-project/client/components/app.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import Nav from './nav';
+import Header from './header';
+import Footer from './footer';
 
 export default class App extends Component { // eslint-disable-line
   render() {
     return (
       <div>
+        <Header />
         <Nav />
         {this.props.children}
+        <Footer />
       </div>
     );
   }
---
+++ flexbox/wesbos/project/01-project/client/components/header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default () => {
+  return (
+    <div className="top">
+      <h1><a href="#">What The Flexbox?!</a></h1>
+    </div>
+  );
+};
