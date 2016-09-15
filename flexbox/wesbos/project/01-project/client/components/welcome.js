4175916679d7ace3471cf8a1aac3a77df8c5f191
SOSANA <thesosana@gmail.com>
2016-09-15 14:48:25 -0600

    updating section element for second hero picture

--- flexbox/wesbos/project/01-project/client/components/welcome.js
+++ flexbox/wesbos/project/01-project/client/components/welcome.js
@@ -4,12 +4,16 @@ export default () => {
   return (
     <div>
       <section className="hero">
-        <img src="http://lorempixel.com/1000/600/abstract" alt="hero" />
+        <img src="/img/abstractTest.png" role="presentation" />
       </section>
 
       <section className="details">
         <p>A simple video course to help you master FlexBox.</p>
         <p>Sign up today to grab all the videos and exercises!</p>
+      </section>
+
+      <section className="hero">
+        <img src="https://static.pexels.com/photos/20974/pexels-photo.jpg" role="presentation" />
       </section>
 
       <section className="signup">
