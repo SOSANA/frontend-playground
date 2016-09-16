bf2f4b088b4d46b9ef4f9483f4544476a266c1fd
SOSANA <thesosana@gmail.com>
2016-09-16 14:47:10 -0600

    removing inline styles in app.js

--- flexbox/wesbos/project/003-pricingGrid/client/components/app.js
+++ flexbox/wesbos/project/003-pricingGrid/client/components/app.js
@@ -1,14 +1,9 @@
 import React, { Component } from 'react';
 
-const h3Style = {
-  textAlign: 'center',
-};
-
 export default class App extends Component { // eslint-disable-line
   render() {
     return (
       <div>
-        <h3 style={h3Style}>Express, React, Redux, and Webpack Starter App</h3>
         {this.props.children}
       </div>
     );
