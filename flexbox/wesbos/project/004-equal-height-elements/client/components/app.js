2ad37cb91999d1d89aa156dbc118b5e482342800
SOSANA <thesosana@gmail.com>
2016-09-20 17:21:16 -0600

    adding project for working with equal height elements with flexbox

---
+++ flexbox/wesbos/project/004-equal-height-elements/client/components/app.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+export default class App extends Component { // eslint-disable-line
+  render() {
+    return (
+      <div>
+        {this.props.children}
+      </div>
+    );
+  }
+}
+
+App.propTypes = {
+  children: React.PropTypes.node,
+};
---
+++ flexbox/wesbos/project/004-equal-height-elements/client/components/welcome.js
@@ -0,0 +1,45 @@
+
+import React from 'react';
+
+export default () => {
+  return (
+    <div className="elements">
+
+      <div className="item">You can tell the world you never was my girl</div>
+      <div className="item">You can burn my clothes when I'm gone</div>
+      <div className="item large">Or you can tell your friends just what a fool I've been</div>
+      <div className="item">And </div>
+
+      <div className="item">You </div>
+      <div className="item small">You </div>
+      <div className="item">Or you can tell my lips to tell my fingertips</div>
+      <div className="item small">They won't be reaching out for you no more</div>
+
+      <div className="item large">But don't tell my heart my achy breaky heart</div>
+      <div className="item">I just don't think he'd understand</div>
+      <div className="item small">And if you tell my heart</div>
+      <div className="item large">my achy breaky heart</div>
+      <div className="item">He might blow up and kill this man</div>
+
+      <div className="item">[guitar]</div>
+
+      <div className="item">You can tell your ma I moved to Arkansas</div>
+      <div className="item large">You can tell your dog to bite my leg</div>
+      <div className="item">Or tell your brother Cliff whose fist can can tell my lip</div>
+      <div className="item small">He never really liked me anyway</div>
+
+      <div className="item">Or tell your Aunt Louise tell anything you please</div>
+      <div className="item">Myself already knows I'm not okay</div>
+      <div className="item large">Or you can tell my eyes to watch out for my mind</div>
+      <div className="item">It might be walking out on me today</div>
+
+      <div className="item">Don't tell my heart my achy breaky heart...</div>
+
+      <div className="item">[guitar]</div>
+
+      <div className="item">Don't tell my heart my achy breaky heart...</div>
+      <div className="item">Don't tell my heart my achy breaky heart...</div>
+
+    </div>
+  );
+};
