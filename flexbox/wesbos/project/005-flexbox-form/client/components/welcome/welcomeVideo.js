1f7c49d93897c30f9b196dd484aae0198a7b5322
SOSANA <thesosana@gmail.com>
2016-09-21 16:22:53 -0600

    updating video background styling, and updating src and css class for video

--- flexbox/wesbos/project/005-flexbox-form/client/components/welcome/welcomeVideo.js
+++ flexbox/wesbos/project/005-flexbox-form/client/components/welcome/welcomeVideo.js
@@ -3,8 +3,8 @@ import React from 'react';
 export default () => {
   return (
     <video
-      className="dog"
-      src="https://s3-us-west-2.amazonaws.com/coverr/mp4/Coverr-Lulu.mp4"
+      className="city"
+      src="https://s3-us-west-2.amazonaws.com/coverr/mp4/Busy-City.mp4"
       autoPlay loop
     />
   );
